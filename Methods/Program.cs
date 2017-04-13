5c96b3572cf6371d9d7220832decded25222092a
BrettMerri <the.b.merri@gmail.com>
2017-04-13 12:59:15 -0400

    initial commit

---
+++ Methods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = getValidInteger();
+            printNumbers(i);
+        }
+
+        public static int getValidInteger()
+        {
+            int input;
+
+            Console.Write("Please enter and integer: ");
+
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("The input is not an integer! Please enter an integer");
+            }
+
+            return input;
+        }
+        public static void printNumbers(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
